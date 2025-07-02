2f259140a6a8c563dc7b8a50e627c72fca6a0291
bigpete <petrvelky@gmail.com>
2025-07-02 21:37:25 +0200

    feat: added database module

---
+++ apps/api/src/database/database.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule, // make sure ConfigService is available
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+    }),
+  ],
+  exports: [MongooseModule],
+})
+export class DatabaseModule {}
